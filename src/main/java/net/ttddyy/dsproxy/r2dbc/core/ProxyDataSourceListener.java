3ec840c93d93f558190580216b38b8208f4c6b0a
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2018-10-23 18:15:48 -0700

    Add beforeMethod/afterMethod on listener

--- src/main/java/net/ttddyy/dsproxy/r2dbc/core/ProxyDataSourceListener.java
+++ src/main/java/net/ttddyy/dsproxy/r2dbc/core/ProxyDataSourceListener.java
@@ -1,16 +1,49 @@
 package net.ttddyy.dsproxy.r2dbc.core;
 
+import io.r2dbc.spi.Batch;
+import io.r2dbc.spi.Statement;
+
 /**
+ * Listener interface that is called by proxy on {@link io.r2dbc.spi.Connection},
+ * {@link io.r2dbc.spi.Statement}, or {@link io.r2dbc.spi.Batch}.
  *
  * @author Tadaya Tsuyukubo
  */
 public interface ProxyDataSourceListener {
 
-    // TODO: beforeMethod, afterMethod
+    /**
+     * Called before every invocation of methods.
+     *
+     * @param executionInfo method execution context
+     */
+    default void beforeMethod(MethodExecutionInfo executionInfo) {
+    }
 
+    /**
+     * Called after every invocation of methods.
+     *
+     * @param executionInfo method execution context
+     */
+    default void afterMethod(MethodExecutionInfo executionInfo) {
+    }
+
+    /**
+     * Called before execution of query.
+     * <p>
+     * Query execution is {@link Batch#execute()} or {@link Statement#execute()}.
+     *
+     * @param execInfo query execution context
+     */
     default void beforeQuery(ExecutionInfo execInfo) {
     }
 
+    /**
+     * Called after execution of query.
+     * <p>
+     * Query execution is {@link Batch#execute()} or {@link Statement#execute()}.
+     *
+     * @param execInfo query execution context
+     */
     default void afterQuery(ExecutionInfo execInfo) {
     }
 
